d7e929d88326fe6ce8b0067ca192f4d2fc2b5f59
Cetheres Alexis <cetherea@student.21-school.ru>
2026-09-12 14:30:15 +0300

    add is less in decimal.h

--- src/s21_decimal.h
+++ src/s21_decimal.h
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
-///основная структура(массив int из 4 чисел, где первые 3 ячейки типа int по 32 бита каждая - основное число; последняя ячейка - служебная)
-typedef struct {
+/// основная структура(массив int из 4 чисел, где первые 3 ячейки типа int по 32 бита каждая - основное число; последняя ячейка - служебная)
+typedef struct
+{
     int bit[4];
-}s21_decimal;
+} s21_decimal;
 
-
-///расширенная структура для промежуточных вычислений
-typedef struct {
+/// расширенная структура для промежуточных вычислений
+typedef struct
+{
     unsigned int bits[6]; // 6 элементов по 32 бита = 192 бита мантиссы
     int scale;            // Масштаб (0..56 в промежуточных вычислениях)
     int sign;             // Знак (0 или 1)
 } s21_big_decimal;
 
 int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
+int s21_is_less(s21_decimal value_1, s21_decimal value_2);
