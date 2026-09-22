89f90efba58f01640f5a927bff16f08375a78bb5
Cetheres Alexis <cetherea@student.21-school.ru>
2026-09-22 19:10:50 +0300

    переставил функцию is greater or equal big внутрь обьявления макроса

--- src/helpers/s21_utils.h
+++ src/helpers/s21_utils.h
@@ -46,6 +46,5 @@ s21_decimal abs_decimal(s21_decimal b); // модуль decimal
 
 void sub_process(s21_big_decimal b_1, s21_big_decimal b_2, s21_big_decimal *result_big); //функция для разности decimal не смотрит на знаки decimal
 void add_process(s21_big_decimal b_1, s21_big_decimal b_2, s21_big_decimal *result_big); //функция для сложения decimal не смотрит на знаки decimal
-
-#endif
 int is_greater_or_equal_big(s21_big_decimal b_1, s21_big_decimal b_2);
+#endif
