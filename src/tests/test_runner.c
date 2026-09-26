4179a70aafa6e3db6c6e0ca17320797a179abefc
Cetheres Alexis <cetherea@student.21-school.ru>
2026-09-26 19:29:15 +0300

    tests for div operation

--- src/tests/test_runner.c
+++ src/tests/test_runner.c
@@ -7,6 +7,7 @@ int main()
     srunner_add_suite(sr, s21_sub_suite());
     srunner_add_suite(sr, suite_comparison());
     srunner_add_suite(sr, s21_mul_suite());
+    srunner_add_suite(sr, s21_div_suite());
     srunner_run_all(sr, CK_NORMAL);
 
     int failed = srunner_ntests_failed(sr);
